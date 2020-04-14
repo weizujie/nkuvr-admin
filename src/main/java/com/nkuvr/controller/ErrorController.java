7ced731b4160b974b90f16c4b968670719424e8a
byojiaoxianz7 <byaojiaoxianz7@outlook.com>
2020-04-14 18:52:14 +0800

    Update README.md

---
+++ src/main/java/com/nkuvr/controller/ErrorController.java
@@ -0,0 +1,10 @@
+package com.nkuvr.controller;
+
+/**
+ * @Author: weizujie
+ * @Date: 2020/4/14
+ * @Version: 1.0
+ * @Github: https://github.com/byojiaoxianz7
+ */
+public class ErrorController {
+}
--- src/main/java/com/nkuvr/pojo/User.java
+++ src/main/java/com/nkuvr/pojo/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.transaction.Transactional;
 
 /**
  * @Author: weizujie
@@ -25,4 +26,5 @@ public class UserEntity extends BaseEntity {
     private Integer stuNum;
     private String realName;
     private String email;
+    private String perms;
 }
